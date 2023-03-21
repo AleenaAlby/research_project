107e1350d771d1f1b6a83d260fd4b320cb047c45
Aleena Alby <aleenaalby20@gmail.com>
2023-03-21 16:03:19 +0000

    changes

--- tasks_app/serializers.py
+++ tasks_app/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Task, Tile
 
 class TaskSerializer(serializers.ModelSerializer):
-    tile = serializers.PrimaryKeyRelatedField(queryset=Tile.objects.all())
     class Meta:
         model = Task
         fields = '__all__'
